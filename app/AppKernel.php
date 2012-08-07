4dae598c94f877ffe065cf0c75e4d7fd5faea17c
Marko Jovanovic <markozjovanovic@gmail.com>
2012-08-07 14:51:29 +0200

    Added Stof/Gedmo doctrine extensions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new MZJ\YabBundle\MZJYabBundle(),
         );
 
