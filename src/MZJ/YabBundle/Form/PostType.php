b978b03f073d1b7851923439ecce3cbc9c441524
Marko Jovanovic <markozjovanovic@gmail.com>
2012-08-08 11:56:14 +0200

    Added require => false for boolean (enabled, commentsEnabled) fields in PostType

--- src/MZJ/YabBundle/Form/PostType.php
+++ src/MZJ/YabBundle/Form/PostType.php
@@ -14,8 +14,8 @@ class PostType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('created_at')
-            ->add('enabled')
-            ->add('commentsEnabled')
+            ->add('enabled', null, array('required' => false))
+            ->add('commentsEnabled', null, array('required' => false))
             ->add('categories')
             ->add('tags')
         ;
