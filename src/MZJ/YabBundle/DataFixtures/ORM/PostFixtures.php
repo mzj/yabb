c4210ab87891f5f7e548f1cd5f54208c929d15e5
Marko Jovanovic <markozjovanovic@gmail.com>
2012-08-10 16:08:41 +0200

    Working on design

--- src/MZJ/YabBundle/DataFixtures/ORM/PostFixtures.php
+++ src/MZJ/YabBundle/DataFixtures/ORM/PostFixtures.php
@@ -47,19 +47,29 @@ class PostFixtures extends AbstractFixture implements OrderedFixtureInterface, C
         
         $post1 = new Post();
         $post1->setTitle('My First Post');
-        $post1->setAbstract('Like I said in the title - this is my first post. Keep reading...');
+        $post1->setAbstract('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
         $post1->setContent('Like I said in the title - this is my first post. Keep reading...');
         $post1->addCategorie($manager->merge($this->getReference('tshirts')));
         
         $post2 = new Post();
         $post2->setTitle('Hello Readers');
-        $post2->setAbstract('Well what is new with you?');
+        $post2->setAbstract('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
+            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ');
         $post2->setContent('Like I said in the title - this is my first post. Keep reading...');
         $post2->addCategorie($manager->merge($this->getReference('shoes')));
         
         $post3 = new Post();
         $post3->setTitle('Learning Scala');
-        $post3->setAbstract('Few days in and still fun.');
+        $post3->setAbstract('At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos 
+            dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, 
+            id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi 
+            optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.');
         $post3->setContent('Like I said in the title - this is my first post. Keep reading...');
         $post3->addCategorie($manager->merge($this->getReference('pants')));
         
